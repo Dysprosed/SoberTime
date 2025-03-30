997f617658aec8a8e1fccce538935c5dd1f04ad7
Dysprosed <dysprosed@gmail.com>
2025-03-29 19:15:26 -0500

    Update LicenseDialogFragment to change license text to GNU General Public License v3.0 and refine acknowledgments

--- app/src/main/java/com/example/sobertime/LicenseDialogFragment.java
+++ app/src/main/java/com/example/sobertime/LicenseDialogFragment.java
@@ -46,26 +46,20 @@ public class LicenseDialogFragment extends DialogFragment {
     
     private String getLicenseText() {
         return "# New Dawn: Sobriety Tracking App\n\n" +
-                "## Open Source License\n\n" +
-                "Copyright (c) 2025 [Your Name]\n\n" +
-                "Permission is hereby granted, free of charge, to any person obtaining a copy " +
-                "of this software and associated documentation files (the \"Software\"), to deal " +
-                "in the Software without restriction, including without limitation the rights " +
-                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell " +
-                "copies of the Software, and to permit persons to whom the Software is " +
-                "furnished to do so, subject to the following conditions:\n\n" +
-                "The above copyright notice and this permission notice shall be included in all " +
-                "copies or substantial portions of the Software.\n\n" +
-                "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR " +
-                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
-                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE " +
-                "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " +
-                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, " +
-                "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE " +
-                "SOFTWARE.\n\n" +
+                "## GNU General Public License v3.0\n\n" +
+                "Copyright (c) 2025\n\n" +
+                "This program is free software: you can redistribute it and/or modify " +
+                "it under the terms of the GNU General Public License as published by " +
+                "the Free Software Foundation, either version 3 of the License, or " +
+                "(at your option) any later version.\n\n" +
+                "This program is distributed in the hope that it will be useful, " +
+                "but WITHOUT ANY WARRANTY; without even the implied warranty of " +
+                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the " +
+                "GNU General Public License for more details.\n\n" +
+                "You should have received a copy of the GNU General Public License " +
+                "along with this program. If not, see <https://www.gnu.org/licenses/>.\n\n" +
                 "## Acknowledgments\n\n" +
-                "- This application was developed with assistance from AI technologies, " +
-                "specifically Claude from Anthropic.\n\n" +
+                "- This application was developed with assistance from Claude AI by Anthropic.\n\n" +
                 "- Special thanks to the open source community for their invaluable tools " +
                 "and libraries that made this project possible.\n\n" +
                 "- Icons and visual elements are created using Android's standard resources " +
@@ -76,7 +70,7 @@ public class LicenseDialogFragment extends DialogFragment {
                 "- No personal information is collected or transmitted\n" +
                 "- Backup files are saved to your device storage and are not shared\n" +
                 "- No analytics or tracking is implemented\n\n" +
-                "This app is provided for personal use to assist with sobriety tracking and " +
+                "This app is provided for sobriety tracking assistance and " +
                 "is not intended as a substitute for professional medical advice, diagnosis, or treatment.";
     }
 }

cd4458feb23fd749b7d396b03be9f1d1c9d0d0c6
Dysprosed <dysprosed@gmail.com>
2025-04-20 20:15:01 -0500

    Add logging import to BackupRestoreActivity for debugging purposes

--- app/src/main/java/com/example/sobertime/BackupRestoreActivity.java
+++ app/src/main/java/com/example/sobertime/BackupRestoreActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
